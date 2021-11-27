b8a5a0d214ca378ddac7daa8be6fb5414bcbdeae
supechicken <supechicken666@gmail.com>
2021-11-28 01:33:29 +0800

    Update sw.js

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,8 +1,16 @@
 // sw.js: Service Worker script
 
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('v1').then( (cache) => {
+      return cache.add('failed.html');
+    })
+  );
+});
+
 self.addEventListener('fetch', (req) => {
   req.respondWith(fetch(e.request.url)).catch (e) {
     console.err('connection error: ', e);
-    return caches.match('offline.html');
+    return caches.match('failed.html');
   }
 });
