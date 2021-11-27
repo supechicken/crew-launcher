7f46826192b3b8776d506b1d60079be54720c11a
supechicken <supechicken666@gmail.com>
2021-11-28 01:26:04 +0800

    Update sw.js

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,37 +1,8 @@
 // sw.js: Service Worker script
 
 self.addEventListener('fetch', (req) => {
-  try {
-    fetch(e.request.url).then( (e) => {
-      req.respondWith(e.blob);
-    });
-  } catch (e) {
+  req.respondWith(fetch(e.request.url)).catch (e) {
     console.err('connection error: ', e);
-    req.respondWith(`
-<html>
-  <style>
-    html, body {
-      height: 100%;
-    }
-
-    body {
-      background-color: white;
-      color: black;
-      font-size: 20px;
-      font-family: Arial;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-    }
-    
-    h1, h2, h3 {
-      font-family: Arial;
-    }
-  </style>
-  <body>
-    <h1>Cannot connect to host!</h1>
-    <h2>Please check if the launcher daemon is started correctly</h2>
-  </body>
-</html>
-`)});
+    return caches.match('offline.html');
+  }
+}
