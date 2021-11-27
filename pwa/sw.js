ba680a14b542d9023dbf61221293a3b54d163d04
supechicken <supechicken666@gmail.com>
2021-11-28 00:38:04 +0800

    Update sw.js

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,4 +1,37 @@
 // sw.js: Service Worker script
 
-// for matching the installation criteria
-self.addEventListener('fetch', (e) => {});
+self.addEventListener('fetch', (req) => {
+  try {
+    fetch(e.request.url).then( (e) => {
+      req.respondWith(e.blob);
+    });
+  } catch (e) {
+    console.err('connection error: ', e);
+    req.respondWith(`
+<html>
+  <style>
+    html, body {
+      height: 100%;
+    }
+
+    body {
+      background-color: white;
+      color: black;
+      font-size: 20px;
+      font-family: Arial;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+    }
+    
+    h1, h2, h3 {
+      font-family: Arial;
+    }
+  </style>
+  <body>
+    <h1>Cannot connect to host!</h1>
+    <h2>Please check if the launcher daemon is started correctly</h2>
+  </body>
+</html>
+`)});
