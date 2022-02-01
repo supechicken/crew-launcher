6e68671358f145fa2ed32d333923d5b3e38a6b90
supechicken <supechicken666@gmail.com>
2022-02-01 23:00:28 +0800

    Update icon_finder.rb

--- lib/icon_finder.rb
+++ lib/icon_finder.rb
@@ -1,49 +1,51 @@
 def magick_installed?
-  return system('which convert > /dev/null 2>&1')
+  return system('command -v convert', out: '/dev/null')
 end
 
-def img_size (icon, w_only: false) # return size of the icon
-  if w_only then format = '%w' else format = '%wx%h' end
+def getImgSize (icon, width_only: false)
+  # img_size: return size of the icon
+  format = (width_only)? '%w' : '%wx%h'
   return `identify -format '#{format}' #{icon}`.chomp
 end
 
-def convert_img (icon, size = '512x512') # convert icon to .png format
-  basename = File.basename(icon, '.*')
-  output = "#{ICONDIR}/#{basename}.png"
+def convert_img (imgFile, convertedSize = '512x512') # convert icon to .png format
+  imgName = File.basename(imgFile, '.*')
+  outImg = "#{ICONDIR}/#{imgName}.png"
 
-  system 'convert', '-resize', size, icon, output, exception: true
-  return output, size, 'image/png'
+  system 'convert', '-resize', convertedSize, imgFile, outImg, exception: true
+  return outImg, convertedSize, 'image/png'
 end
 
 module IconFinder
   def self.find(pkgName, iconName) # find an icon from paths in package's filelist
-    # TODO: support non-chromebrew apps
-    fileList = File.readlines("#{CREW_PREFIX}/etc/crew/meta/#{pkgName}.filelist", chomp: true)
-    svg = fileList.grep(/#{iconName}\.svg$/)[0]
-    xpm = fileList.grep(/#{iconName}\.xpm$/)[0]
-    png = fileList.grep(/#{iconName}\.png$/).sort_by do |path|
-      # TODO: always use theme if available
-      # get the highest resolution file
-      path[/\d+x/].to_i
-    end[-1]
+    matchedIcon = Dir[*IconSearchGlob.map {|path| path % pkgName}].sort_by do |icon|
+                    # priority: '.svg' > '.png' > '.xpm' > Chromebrew Icon
+                    case File.extname(icon)
+                    when '.svg'
+                      3
+                    when '.png'
+                      pngSize = getImgSize(icon, w_only: true).to_i
+                      # assign priority based on the image size
+                      "2.#{pngSize.rjust(4, '0')}".to_f
+                    when '.xpm'
+                      0
+                    end
+                  end[-1]
 
-    # priority: 'app.svg' > 'app.png' > 'app.xpm' > Chromebrew Icon
-    if svg
-      iconPath, iconSize, iconMime = svg, '512x512', 'image/svg+xml'
-    elsif magick_installed? and png and img_size(png, w_only: true).to_i >= 144
-      # use the png if it meets the minimum requirement of PWA (must be >= 144x144px)
-      pngSize = img_size(png)
-      iconPath, iconSize, iconMime = png, pngSize, 'image/png'
-    elsif png
-      # if size < 144x144px, resize it
-      iconPath, iconSize, iconMime = convert_img(png)
-    elsif magick_installed? and xpm
-      # convert the .xpm file to .png as .xpm is not supported by chrome
-      iconPath, iconSize, iconMime = convert_img(xpm)
-    else
-      error 'Unable to find an icon :/'
-      iconPath, iconSize, iconMime = CREWICON, '546x546', 'image/png'
-    end
+    # convert the XPM file to PNG as XPM is not supported by chrome
+    # if the icon size is smaller than 144x144px, resize it to meets the minimum requirement of PWA icons
+    iconPath, iconSize, iconMime = if matchIcon and matchIcon !~ /\.svg$/ and \
+                                        (
+                                          getImgSize(matchedIcon, w_only: true).to_i >= 144 or \
+                                          matchedIcon =~ /\.xpm$/
+                                        )
+                                     convert_img(matchedIcon)
+                                   elsif matchIcon
+                                     matchedIcon, getImgSize(matchedIcon), MimeType[ File.extname(matchedIcon) ]
+                                   else
+                                     error 'Unable to find an icon :/'
+                                     iconPath, iconSize, iconMime = CREWICON, '546x546', 'image/png'
+                                   end
 
     # remove duplicate slash in path
     iconPath.squeeze!('/')
