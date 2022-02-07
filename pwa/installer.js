a987a4386c9a3fddaca12cfac92eb6496ba21621
supechicken <supechicken666@gmail.com>
2022-02-07 22:39:44 +0800

    Update installer.js

--- pwa/installer.js
+++ pwa/installer.js
@@ -1,12 +1,19 @@
+const params = new URLSearchParams(document.location.search);
+
+// set manifest path
+let linkElement = document.createElement('link');
+linkElement.rel = 'manifest';
+linkElement.setAttribute('href', `/installer/manifest.webmanifest?entryFile=${params.get('entryFile')}`);
+document.head.appendChild(linkElement);
+
+
+
 navigator.serviceWorker.register('./sw.js')
            
 self.addEventListener('appinstalled', (event) => {
   // tell socket server to stop
-  fetch('/stop', { method: 'POST' }).then( () => {
-    // show close message
-    document.getElementById('installBut').style.visibility = 'hidden';
-    document.getElementById('closeMsg').style.visibility = 'visible';
-  });
+  document.getElementById('installBut').style.visibility = 'hidden';
+  document.getElementById('closeMsg').style.visibility = 'visible';
 });
 
 self.addEventListener('beforeinstallprompt', (e) => {
