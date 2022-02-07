688d805069932a7862b5c6ab3ebedf1a3e9d65c5
supechicken <supechicken666@gmail.com>
2022-02-07 18:34:08 +0800

    Update desktop_file.rb

--- lib/desktop_file.rb
+++ lib/desktop_file.rb
@@ -1,40 +1,21 @@
 module DesktopFile
-  def self.find(pkg) # find a .desktop file from package's filelist
-    abort "Package #{pkg} isn't installed.".lightred unless File.exist?("#{CREW_PREFIX}/etc/crew/meta/#{pkg}.filelist")
-
-    results = `grep -m1 '\.desktop$' #{CREW_PREFIX}/etc/crew/meta/#{pkg}.filelist`.chomp
-    unless results.empty?
-      Verbose.puts "Desktop Entry File found: #{results}"
-      return results
-    else
-      abort "Cannot find an `.desktop` file for #{pkg} :/".lightred
-    end
-  end
-
   def self.parse(path) # parse .desktop file into hash
-    file = File.read(path, encoding: Encoding::UTF_8)
+    fileIO = File.open(entryFile, 'r')
+    parsedEntry = Hash.new
 
-    # split groups
-    file.split("\n\n").map do |group|
-      group.strip!
-      header = group.lines[0][/\[(.+)\]/, 1]
-      entries = group.scan(/^(.+)=(.*)/)
-
-      if Locale
-        # get all localized entries
-        localizedKeys = entries.keys.grep(/\[(#{Locale[:lang]})(_#{Locale[:country]})?(@#{Locale[:modifier]})?\]$/)
-
-        entries.map! do |k, v|
-          # replace values with localized one if available
-          matchedKey = localizedKeys.grep(/^#{k}/)[0]
-          v = entries[matchedKey] if matchedKey
-
-          [ k, v ]
-        end
+    while ( line = fileIO.gets(chomp: true) )
+      case line[0]
+      when '#', "\n"
+        next
+      when '['
+        currentGroup = line[/^\[(.+)\]$/, 1]
+        parsedEntry[currentGroup] = Hash.new
+      else
+        k, v = line.split('=', 2)
+        parsedEntry[currentGroup][k] = v
       end
+    end
 
-      [ header, entries.to_h ]
-    end.to_h
-    # return in `{group => {entry}, group => {entry}...}`
+    return parsedEntry
   end
 end
